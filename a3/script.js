ddfb7b5fe69fbfd0ec1c3d7d50e0b7f2e5fccc01
analgro <166257266+analgro@users.noreply.github.com>
2024-05-11 21:46:45 -0700

    Update script.js

--- a3/script.js
+++ a3/script.js
@@ -1,1 +1,38 @@
+function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var upRhombus = generateRhombus(pHeight, pColorEven, pColorOdd, pSymbol);
+    var downRhombus = generateRhombus(pHeight, pColorEven, pColorOdd, pSymbol, true); // Generate the bottom half with reverse ordering
+
+    document.getElementById("upRight").innerHTML = upRhombus;
+    document.getElementById("downRight").innerHTML = downRhombus;
+}
+
+function generateRhombus(pHeight, pColorEven, pColorOdd, pSymbol, reverse = false) {
+    var rhombus = "";
+
+    for (var i = 1; i <= pHeight; i++) {
+        var spaces = pHeight - i;
+        var symbols = i * 2 - 1;
+
+        rhombus += "<p style='text-align: center;'>";
+
+        // Add leading spaces
+        for (var j = 0; j < spaces; j++) {
+            rhombus += "&nbsp;&nbsp;";
+        }
+
+        // Add symbols
+        for (var k = 0; k < symbols; k++) {
+            rhombus += "<span style='color:" + ((k % 2 === 0) ? pColorEven : pColorOdd) + ";'>" + pSymbol + "</span>";
+        }
+
+        rhombus += "</p>";
+    }
+
+    if (reverse) {
+        // Reverse the order of lines for bottom half
+        rhombus = rhombus.split("</p>").reverse().join("</p>");
+    }
+
+    return rhombus;
+}
 
