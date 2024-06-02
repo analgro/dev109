b99aa17a71aee8b7e4c6e600273b8136bf69f7a4
analgro <166257266+analgro@users.noreply.github.com>
2024-06-01 21:38:00 -0700

    Update script.js

--- a5/script.js
+++ a5/script.js
@@ -1,52 +1,58 @@
-const images = [
-    { src: 'a5/cooldawg.jpg', desc: 'Description for Image 1' },
-    { src: 'a5/confuseddawg.jpg', desc: 'Description for Image 2' },
-    { src: 'a5/bignosedawg.jpg', desc: 'Description for Image 3' },
-    { src: 'a5/topofdawg.jpg', desc: 'Description for Image 4' },
-    { src: 'a5/thinkingdawg.jpg', desc: 'Description for Image 5' }
+var myImages = [
+    "images/cooldawg.jpg",
+    "images/cunfuseddawg.jpg",
+    "images/topofdawg.jpg",
+    "images/bignosedawg.jpg",
+    "images/thinkingdawg.jpg"
 ];
 
-let currentIndex = 0;
-let interval;
+var captionImages = [
+    "Dog is looking down towards the camera and looks super cool",
+    "Dog is looking at the camera in a confused manner",
+    "An image of only the top of the dogs head",
+    "Dog with a big nose",
+    "Dog is thinking of life choices"
+];
 
-const slide = document.getElementById('slide');
-const description = document.getElementById('description');
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
+var index = 0;
 
-function showImage(index) {
-    slide.src = images[index].src;
-    description.textContent = images[index].desc;
+function updateImage() {
+    document.getElementById("slideshow").src = myImages[index];
+    document.getElementById("slideshow").alt = captionImages[index];
+    document.getElementById("caption").textContent = captionImages[index];
 }
 
-function showNextImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    showImage(currentIndex);
+function next() {
+    if (myImages.length == index + 1) {
+        index = 0;
+    } else {
+        index++;
+    }
+    updateImage();
 }
 
-function showPreviousImage() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    showImage(currentIndex);
+function back() {
+    if (index === 0) {
+        index = myImages.length - 1;
+    } else {
+        index--;
+    }
+    updateImage();
 }
 
-function resetInterval() {
-    clearInterval(interval);
-    interval = setInterval(showNextImage, 3000);
+var nextButton = document.getElementById("next");
+var previousButton = document.getElementById("previous");
+
+previousButton.addEventListener("click", back, false);
+nextButton.addEventListener("click", next, false);
+
+function autoSlide() {
+    if (document.getElementById("auto").checked) {
+        next();
+    }
 }
 
-prevButton.addEventListener('click', () => {
-    showPreviousImage();
-    resetInterval();
-});
-
-nextButton.addEventListener('click', () => {
-    showNextImage();
-    resetInterval();
-});
-
-// Start the slideshow
-interval = setInterval(showNextImage, 3000);
+setInterval(autoSlide, 2000);
 
 // Show the first image
-showImage(currentIndex);
-
+updateImage();
